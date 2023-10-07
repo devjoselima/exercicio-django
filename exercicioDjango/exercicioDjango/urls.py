408b9de407262528147e06230f9a4339927a58e7
Jose Lima <jalimag0206@gmail.com>
2023-10-07 12:27:45 -0300

    finishing exercise django admin

--- exercicioDjango/exercicioDjango/urls.py
+++ exercicioDjango/exercicioDjango/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path('HOME', include('projetoDjango.urls')),
+    path('admin/', admin.site.urls),
+    path('home', include('projetoDjango.urls')),
 ]  
